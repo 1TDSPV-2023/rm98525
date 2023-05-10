5980125f2e537b5c619347833f17d7cdb05a3e74
RaphaPab <rm98525@fiap.com.br>
2023-05-09 22:50:06 -0300

    catch e throw

--- js/login.js
+++ js/login.js
@@ -11,32 +11,35 @@ const usuario2 = {
     senhaUsuario : "12345"
 }
 
-console.log(usuario1.nomeUsuario)
-console.log(usuario2.nomeUsuario)
-usuario2.nomeUsuario = "Juquinha";
-console.log(usuario2.nomeUsuario);
-
 //LISTA DE USUÁRIOS
 let listaDeUsuarios = [];
 listaDeUsuarios.push(usuario1);
 listaDeUsuarios.push(usuario2);
-
-
-
-
 
 addEventListener("click", (evento)=>{
      
     let userInput = document.querySelector("#idUser"); 
     let passInput = document.querySelector("#idPass"); 
 
-    if(evento.target.id == "btnSubmit"){
+        if(evento.target.id == "btnSubmit"){
         
-        if(userInput.value == "pf0670" && passInput.value == "12345"){
-            console.log("USUÁRIO VALIDADO!");
+        try{
+                listaDeUsuarios.forEach((usuario)=>{
+
+                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                        throw "USUÁRIO VALIDADO!";
+                    }
+                });
+
+                throw "USUÁRIO OU SENHA INCORRETOS!";
+
+    }catch(msg){
+        if(msg == "USUÁRIO VALIDADO!"){
+            console.log("USUÁRIO VALIDADO!")
         }else{
             console.log("USUÁRIO OU SENHA INCORRETOS!");
         }
-
     }
+
+  }
 });
